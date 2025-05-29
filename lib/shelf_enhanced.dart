5ba176a22866b5d50891ff7885a887a043bcd37a
Tiago Alves <tiago@softbuilder.com.br>
2025-05-29 20:32:30 -0300

    version: 1.0.4

--- lib/shelf_enhanced.dart
+++ lib/shelf_enhanced.dart
@@ -4,6 +4,9 @@ import 'src/app.dart';
 import 'src/interfaces/app.dart';
 
 export 'package:shelf/shelf.dart';
+export 'package:shelf_cors_headers/shelf_cors_headers.dart';
+export 'package:shelf_proxy/shelf_proxy.dart';
+export 'package:shelf_static/shelf_static.dart';
 
 export 'src/app.dart';
 export 'src/common/field.dart';
